4c126ae489ce50b37c3d3458fdfe471c1dfd968b
Satwik Narlanka <satwnarl@iu.edu>
2018-04-30 18:24:19 -0400

    intermediate commit

--- 101. Symmetric Tree.py
+++ 101. Symmetric Tree.py
@@ -1,3 +1,4 @@
+from collections import deque
 # Definition for a binary tree node.
 class TreeNode:
     def __init__(self, x):
@@ -25,6 +26,31 @@ class Solution:
         else:
             return self.isMirror(root1.left, root2.right) and self.isMirror(root1.right,root2.left)
 
+    def isSymmetricIterative(self,root):
+        if not root:
+            return True
+        q = deque()
+        q.extend([root, '#'])
+        temp = []
+        while True:
+            if len(q) ==1:
+                l = len(temp)//2
+                if temp[:l] != list(reversed(temp[l:])):
+                    return False
+                break
+            node = q.popleft()
+            if node == '#':
+                q.append('#')
+                l = len(temp)//2
+                if temp[:l] != list(reversed(temp[l:])) and l>0:
+                    print(temp)
+                    return False
+                temp = []
+            else:
+                if node != None:
+                    temp.append(node.val)
+                q.extend([node.left,node.right])
+        return True
 
 #testcase
 t = TreeNode(1)
@@ -38,3 +64,4 @@ c2.left = TreeNode(3)
 c2.right = TreeNode(4)
 s = Solution()
 print(s.isSymmetric(t))
+print(s.isSymmetricIterative(t))
