dec7332239d747a0f6e78bd72b8fc70ce8719db2
Satwik Narlanka <satwnarl@iu.edu>
2018-07-14 21:08:45 -0400

    Jewels and stones

---
+++ 771. Jewels and Stones.py
@@ -0,0 +1,17 @@
+class Solution:
+    def numJewelsInStones(self, J, S):
+        """
+        :type J: str
+        :type S: str
+        :rtype: int
+        """
+        ans = 0
+        for stone in S:
+            if stone in J:
+                ans +=1
+        return ans
+
+s = Solution()
+J = "aA"
+S = "aAAbbbb"
+print(s.numJewelsInStones(J,S))
