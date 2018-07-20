eb2a9cbec406b402a91b6d7f90b7db65c17fae74
Satwik Narlanka <9220329+satwiknarlanka@users.noreply.github.com>
2018-07-20 18:32:30 -0400

    https://leetcode.com/problems/baseball-game/

--- 682. Baseball Game.py
+++ 682. Baseball Game.py
@@ -4,33 +4,17 @@ class Solution:
         :type ops: List[str]
         :rtype: int
         """
-        ans = 0
-        stack = []
+        res = []
         for op in ops:
-            if op == '+':
-                points = 0
-                if len(stack)>=2:
-                    points = stack[-1] + stack[-2]
-                    stack.append(points)
-                elif len(stack) == 1:
-                    points = stack[-1]
-                    stack.append(points)
-                else:
-                    points = 0
-                ans += points
+            if op == 'C':
+                res.pop()
             elif op == 'D':
-                points = stack[-1] *2
-                ans += points
-                stack.append(points)
-            elif op == 'C':
-                red = stack[-1]
-                ans -= int(red)
-                stack.pop()
+                res.append(res[-1] * 2)
+            elif op == '+':
+                res.append(res[-1] + res[-2])
             else:
-                ans += int(op)
-                stack.append(int(op))
-        return ans
+                res.append(int(op))
+        return sum(res)
 
 s = Solution()
-
 print(s.calPoints(["5","2","C","D","+"]))
