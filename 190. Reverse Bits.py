444ee9de2092b6c58d37815ba1b378d7bd1bfbd4
satwik narlanka <satwik.602@gmail.com>
2018-07-22 22:59:43 -0400

    https://leetcode.com/problems/reverse-bits/description/

---
+++ 190. Reverse Bits.py
@@ -0,0 +1,14 @@
+class Solution:
+    def reverseBits(self, n):
+        ans = 0
+        for i in range(32):
+            ans += (n & 2**i)>>i
+            ans <<=1 
+        return ans>>1
+
+s = Solution()
+print(s.reverseBits(43261596))
+'''
+one line solution:
+return int(''.join(reversed(bin(n)[2:].zfill(32))),2)
+'''
